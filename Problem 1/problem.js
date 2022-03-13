00c58251df6d52046117e09b20a2f966a080d1bd
rafaapcode <rafaap2003@gmail.com>
2022-03-13 18:06:31 -0300

    Two Problems Resolve

---
+++ Problem 1/problem.js
@@ -0,0 +1,16 @@
+function t() {
+    let arr = [];
+    for (let i = 0; i < 1000; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            arr.push(i);
+            
+        }
+    }
+    let sum = arr.reduce((total, value) => {
+        return total + value;
+    })
+
+    return sum
+}
+
+console.log(t());
---
+++ Problem 7/problem.js
@@ -0,0 +1,19 @@
+let arr = [];
+let pos = 2000000;
+
+for (let i = 2; i <= pos; i++) {
+    let ehprimo = true;
+    for (let d = 2; d < i; d++) {
+        if (i % d === 0) {
+            ehprimo = false;
+        }
+    };
+
+    if (ehprimo) {
+        arr.push(i);
+    };
+    if (arr.length === 10001) {
+        console.log(arr[10000]);
+    }
+
+}
