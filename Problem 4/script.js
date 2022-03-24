8877e3ddcfc8b21e3074a8296e7680c4551d9366
rafaapcode <rafaap2003@gmail.com>
2022-03-24 19:46:26 -0300

    problem 4

---
+++ Problem 4/script.js
@@ -0,0 +1,31 @@
+function isPalindrome(n) {
+
+    // Pegando o número ao contrário
+    let reversed = String(n).split("").reverse().join("");
+    let normal = n;
+
+    return Number(reversed) === n;
+}
+
+function largestPalindrome(num) {
+    let highest = 0;
+
+    // Encontrando o maior número
+    let largestNum = "9";
+    largestNum += Number(largestNum.repeat(num - 1));
+    largestNum = Number(largestNum);
+    for (let i = largestNum; i > 0; i--) {
+        for (let j = largestNum; j > 0; j--) {
+            let product = i * j;
+            if (isPalindrome(product)) {
+                if (product > highest) {
+                    highest = product;
+                    break;
+                }
+            }
+        }
+    }
+    return highest;
+}
+
+console.log(largestPalindrome(3));
