001ad5b090db4b2f495d68e947681ca688a50c82
rafaapcode <rafaap2003@gmail.com>
2022-03-27 07:26:06 -0300

    front - OK

--- Problem 1/script.js
+++ Problem 1/script.js
@@ -1,14 +1,25 @@
-module.exports.t = () => {
+const p1 = document.querySelector(".problem1");
+const r1 = document.querySelector(".r1");
+
+p1.addEventListener("click", () => {
+    r1.innerHTML = `<div class='result1'>${t()}</div>`;
+    setTimeout(() => {
+        r1.innerHTML = ``;
+    }, 10000)
+})
+
+
+function t() {
     let arr = [];
     for (let i = 0; i < 1000; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
             arr.push(i);
-            
+
         }
     }
     let sum = arr.reduce((total, value) => {
         return total + value;
     })
 
-    console.log(sum);
+    return sum;
 }
--- index/index.js
+++
@@ -1,9 +0,0 @@
-const p1 = require("../Problem 1/script");
-const p2 = require("../Problem 2/script");
-const p3 = require("../Problem 3/script");  // 600851475143
-const p4 = require("../Problem 4/script"); // 3
-
-
-
-const p7 = require("../Problem 7/script");
-
