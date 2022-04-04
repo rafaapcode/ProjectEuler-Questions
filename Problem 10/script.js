3a6844ac42dc6ba29a0a09f9f348da546561d5d7
rafaapcode <rafaap2003@gmail.com>
2022-04-04 09:35:22 -0300

    Problem 9 e 10

--- Problem 10/script.js
+++ Problem 10/script.js
@@ -1,27 +1,11 @@
-function primeNum() {
+const p10 = document.querySelector(".problem10");
+const r10 = document.querySelector(".r10");
 
-    let arr = [];
-    let pos = 1999999;
+p10.addEventListener("click", () => {
+    r10.innerHTML = `<div class='result2'>142913828922</div>`;
+    setTimeout(() => {
+        r10.innerHTML = ``;
+    }, 10000)
+})
 
-    for (let i = 0; i <= pos; i++) {
-        let ehprimo = true;
-        for (let d = 0; d < i; d++) {
-            if (i % d === 0) {
-                ehprimo = false;
-            }
-        };
 
-        if (ehprimo) {
-            arr.push(i);
-        };
-    }
-
-    let sum = arr.reduce((acc, value) => {
-        return acc + value;
-    })
-
-    console.log(sum);
-
-}
-
-primeNum();
--- Problem 9/script.js
+++ Problem 9/script.js
@@ -2,7 +2,7 @@ const p9 = document.querySelector(".problem9");
 const r9 = document.querySelector(".r9");
 
 p9.addEventListener("click", () => {
-    r9.innerHTML = `<div class='result2'>a: 200, b: 375, c: 425</div>`;
+    r9.innerHTML = `<div class='result2'>31875000</div>`;
     setTimeout(() => {
         r9.innerHTML = ``;
     }, 10000)
@@ -14,7 +14,7 @@ for (let i = 1; i <= sum / 3; i++) {
     for (let b = i + 1; b <= sum / 2; b++) {
         let c = sum - i - b;
         if (i * i + b * b == c * c) {
-            console.log(i, b, c);
+            console.log(i * b * c);
         }
     }
 }
